2323af7a6976467f8f5bafce45fdd401c9dc894f
cest0g <erwinscholts@gmail.com>
2026-03-22 21:50:16 +0100

    Rebuild

--- Scrolltopbtn.js
+++
@@ -1,9 +0,0 @@
-const scrollTopLink = document.getElementById("scrollTopLink");
-
-scrollTopLink.addEventListener("click", function(event) {
-  event.preventDefault(); // Prevent navigating to index.html
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});
